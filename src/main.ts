14178c42902fa5e1b292300f4a4c7fc19c413a09
t0nxx <mahmoudko1500@hotmail.com>
2024-04-07 14:39:47 +0200

    ✨ Add Database module , with prisma orm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * Swagger Documentation Config
+   */
+  const config = new DocumentBuilder()
+    .setTitle('ra3d task management')
+    .setDescription('The ra3d API description')
+    .setVersion('1.0')
+    .addTag('ra3d')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  /**
+   *  App GlobalPipes
+   */
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(3000);
 }
 bootstrap();
